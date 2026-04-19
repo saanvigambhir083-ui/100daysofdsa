477ccfdcde63ebf64b99db9ac6964a5860b0a527
saanvigambhir083-ui <saanvigambhir083@gmail.com>
2026-04-19 23:25:37 +0530

    Implement cycle detection in linked list

---
+++ day23Q2.c
@@ -0,0 +1,18 @@
+//Given head, the head of a linked list, determine if the linked list has a cycle in it.
+bool hasCycle(struct ListNode *head) 
+{
+    if (head==NULL||head->next==NULL)
+        return false;
+    struct ListNode *slow=head;
+    struct ListNode *fast=head;
+    while (fast!=NULL && fast->next!=NULL) 
+    {
+     slow=slow->next;      
+     fast=fast->next->next;
+        if (slow==fast) 
+        {
+         return true;
+        }
+    }
+    return false;
+}
